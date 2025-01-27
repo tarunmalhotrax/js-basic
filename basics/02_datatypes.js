95c3aa42a582be36940118914a9403d1eaccfcf8
tarunmalhotrax <tarunmalhotra1294@gmail.com>
2025-01-27 15:00:01 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //using node js not browser
+
+//tc39 and mdn are documentation for javascript
+
+let name ="hitesh" //string
+let age = 22 // number
+let loggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean  => true/false
+// null => standlone value // empty value
+// undefined // it is not defined // type
+// symbol => unique
+
+// object 
+
+console.log(typeof "tarun"); // string
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
+
